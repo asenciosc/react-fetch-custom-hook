64554f3adff39c2333cfaf6f030fb21d80460d36
Chris <asenciosc@outlook.com>
2020-12-07 20:05:09 -0700

    prettified

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,18 +1,17 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
 export const useFetch = (url) => {
-    const [state, setState] = useState({data: null, loading: true });
+  const [state, setState] = useState({ data: null, loading: true });
 
-    useEffect(() => {
-        
-        setState({ data:null, loading: true});
+  useEffect(() => {
+    setState({ data: null, loading: true });
 
-        fetch(url)
-            .then(x => x.text())
-            .then(y => {
-                setState({ data: y, loading: false})
-            });
-    }, [url, setState]);
+    fetch(url)
+      .then((x) => x.text())
+      .then((y) => {
+        setState({ data: y, loading: false });
+      });
+  }, [url, setState]);
 
-    return state;
+  return state;
 };
